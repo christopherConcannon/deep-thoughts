143d509a22dc8c0604341f356629a5fe4c4e7048
xtophe xanon <xocon10@gmail.com>
2020-10-05 11:15:21 -0500

    trivial

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -44,8 +44,7 @@ type Query {
 
 type Mutation {
   login(email: String!, password: String!): Auth
-  ${/*addUser(username: String!, email: String!, password: String!): Auth*/''}
-  addUser(username: String!, email: String!, password: String!): User
+  addUser(username: String!, email: String!, password: String!): Auth
   addThought(thoughtText: String!): Thought
   addReaction(thoughtId: ID!, reactionBody: String!): Thought
   addFriend(friendId: ID!): User
